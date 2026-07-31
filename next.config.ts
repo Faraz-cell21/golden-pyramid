1c1b9ca3db834eb919cc51494e214dfc138586fd
Faraz-cell21 <faraz127200@outlook.com>
2026-07-31 10:06:26 +0500

    fixed lint issue

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactCompiler: true,
   devIndicators: false,
+  reactCompiler: true,
 };
 
 export default nextConfig;
